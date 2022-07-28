c245e1941ff512af8b7511fe96066854a7c2b690
Leolira1808 <97995984+Leolira1808@users.noreply.github.com>
2022-07-28 18:13:03 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.BootCamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -16,15 +18,45 @@ public class Main {
         curso2.setDescricao("Discrição curso Python");
         curso2.setCargaHoraria(12);
 
+
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria de java");
         mentoria.setDescricao("Descrição Mentoria curso de java");
         mentoria.setData(LocalDate.now());
 
+        BootCamp bootCamp = new BootCamp();
+        bootCamp.setNome(("BootCamp Java Developer"));
+        bootCamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootCamp.getConteudos().add(curso1);
+        bootCamp.getConteudos().add(curso2);
+        bootCamp.getConteudos().add(mentoria);
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Dev devLeonardo = new Dev();
+        devLeonardo.setNome("Leonardo");
+        devLeonardo.inscreverBootcamp(bootCamp);
+        System.out.println("Conteudo Inscrito Leonardo" + devLeonardo.getConteudosInscricos());
+        devLeonardo.progredir();
+        System.out.println("-");
+        System.out.println("Conteudo Inscrito Leonardo" + devLeonardo.getConteudosInscricos());
+        System.out.println("Conteudo Concluidos Leonardo" + devLeonardo.getConteudosConcluidos());
+        System.out.println("XP: " + devLeonardo.calcularTotalXp());
+
+
+        System.out.println("-----------------");
+
+
+        Dev devIsis = new Dev();
+        devIsis.setNome("Isis Regina");
+        devIsis.inscreverBootcamp(bootCamp);
+        System.out.println("Conteudo Inscrito Isis Regina" + devIsis.getConteudosInscricos());
+        devIsis.progredir();
+        devIsis.progredir();
+        devIsis.progredir();
+        System.out.println("-");
+        System.out.println("Conteudo Inscrito Isis Regina" + devIsis.getConteudosInscricos());
+        System.out.println("Conteudo Inscrito Isis Regina" + devIsis.getConteudosConcluidos());
+        System.out.println("XP:" + devIsis.calcularTotalXp());
+
 
     }
 }
